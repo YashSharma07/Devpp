775e3a4be3d64e137b0d806decd8bb2577e69dd8
Yash Sharma <yashqwer56@gmail.com>
2021-03-06 06:28:31 -0800

    functions

---
+++ function.js
@@ -0,0 +1,14 @@
+
+// function body with no arguement 
+
+// let sayHi = function() {
+//     console.log("hi i am a function without arguement");
+// }
+
+// sayHi();
+
+function sayHi(name) {
+    console.log(name + " says Hiii !!!");
+}
+
+sayHi("Binod");
